efc7cf41d60fac0be03eda215d93b02c7af49189
Ujawal Tiwari <ujawal0011@gmail.com>
2022-12-20 08:28:55 +0530

    set it up some folders MVC structures

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const app = express();
 
 
 
-app.get('/', function(req,res){
-    res.send('<h1>Cool Go</h1>')
-}, function(err){
-    if(err){
-        console.log('Error');
-        
-    }
-})
 
 
+// app.get('/', function(req,res){
+//     res.send('<h1>Cool Go</h1>')
+// }, function(err){
+//     if(err){
+//         console.log('Error');
+
+//     }
+// })
 
 //Setting up my server
 app.listen(port , function(err){
