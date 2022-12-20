3db37d4391859d2ee985aa16fefc26a691554a43
Ujawal Tiwari <ujawal0011@gmail.com>
2022-12-20 08:15:47 +0530

    set it up my server and nodemon

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+
+
+
+
+app.get('/', function(req,res){
+    res.send('<h1>Cool Go</h1>')
+}, function(err){
+    if(err){
+        console.log('Error');
+        
+    }
+})
+
+
+
+//Setting up my server
+app.listen(port , function(err){
+    if(err){
+        console.log('Error in starting the server');
+    }
+    console.log(`Server is running on port ${port}`);
+});
